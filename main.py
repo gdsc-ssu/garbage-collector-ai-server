b9f32b9fb6e89d50b108ef9d0f0e0e43cf2bc8e1
HwanGonJang <myggona@gmail.com>
2023-02-05 10:12:35 +0900

    feat: setup initial classification model

--- main.py
+++ main.py
@@ -2,12 +2,41 @@ import numpy as np
 
 from keras.preprocessing import image
 from keras.models import load_model
+import keras.applications.mobilenet_v2 as mobilenetv2
+import cv2
+from PIL import Image
+import tensorflow as tf
 
-model = load_model('model12.h5')
+categories = {0: 'battery', 1: 'biological', 2: 'glass', 3: 'cardboard', 4: 'clothes', 5: 'green-glass',
+              6: 'metal', 7: 'paper', 8: 'plastic', 9: 'shoes', 10: 'trash',
+              11: 'white-glass'}
 
-trash_img = image.load_img('test-dataset/battery1.jpg')
+def mobilenetv2_preprocessing(img):
+    return mobilenetv2.preprocess_input(img)
 
-print(model.predict(trash_img))
+
+model = load_model('trash_model.h5')
+
+trash_img = Image.open('test-dataset/battery1.jpg')
+
+trash_img = np.asarray(trash_img)
+
+trash_img = tf.image.resize(trash_img, (224, 224))
+
+trash_img = np.reshape(trash_img, (1, 224, 224, 3))
+
+# print(trash_img.shape)
+
+predict = model.predict(trash_img)
+
+# print(predict)
+# print(predict[0][1])
+# print(np.sum(predict[0]))
+
+max = max(predict[0]) * 100
+# max_index = predict[0].where(predict[0] == max)
+
+print(f"이 쓰레기는 {round(max, 2)}% 확률로 {categories[np.argmax(predict[0])]}입니다.")
 
 #
 # x = image.img_to_array(trash_img)
