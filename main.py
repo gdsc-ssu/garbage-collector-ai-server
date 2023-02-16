a83655d6b17bac452d54540640ecb1202c23d24d
HwanGonJang <myggona@gmail.com>
2023-02-16 14:13:52 +0900

    feat: apply fast api

--- main.py
+++ main.py
@@ -1,47 +1,51 @@
 import numpy as np
-
-from keras.preprocessing import image
 from keras.models import load_model
 import keras.applications.mobilenet_v2 as mobilenetv2
-import cv2
-from PIL import Image
 import tensorflow as tf
+
+import io
+from PIL import Image
+from fastapi import FastAPI, File
+
+model = load_model('trash_model.h5')
 
 categories = {0: 'battery', 1: 'biological', 2: 'glass', 3: 'cardboard', 4: 'clothes', 5: 'green-glass',
               6: 'metal', 7: 'paper', 8: 'plastic', 9: 'shoes', 10: 'trash',
               11: 'white-glass'}
 
+
 def mobilenetv2_preprocessing(img):
     return mobilenetv2.preprocess_input(img)
 
 
-model = load_model('trash_model.h5')
+def predict_garbage_type(garbage_image: bytes = File()):
+    img = await garbage_image.read()
+    img = Image.open(io.BytesIO(img))
+    # img = Image.open(garbage_image)
+    img = np.asarray(img)
+    img = tf.image.resize(img, (224, 224))
+    img = np.reshape(img, (1, 224, 224, 3))
 
-trash_img = Image.open('test-dataset/battery1.jpg')
+    predict = model.predict(img)
 
-trash_img = np.asarray(trash_img)
+    prob = max(predict[0]) * 100
+    prob = round(prob, 2)
 
-trash_img = tf.image.resize(trash_img, (224, 224))
+    garbage_category = categories[np.argmax(predict[0])]
 
-trash_img = np.reshape(trash_img, (1, 224, 224, 3))
+    return prob, garbage_category
 
-# print(trash_img.shape)
 
-predict = model.predict(trash_img)
+app = FastAPI()
 
-# print(predict)
-# print(predict[0][1])
-# print(np.sum(predict[0]))
 
-max = max(predict[0]) * 100
-# max_index = predict[0].where(predict[0] == max)
+@app.get("/test")
+async def test():
+    return {"result": "Hello World!"}
 
-print(f"이 쓰레기는 {round(max, 2)}% 확률로 {categories[np.argmax(predict[0])]}입니다.")
 
-#
-# x = image.img_to_array(trash_img)
-# x = np.expand_dims(x, axis=0)
-#
-# images = np.vstack([x])
-# classes = model.predict_classes(images, batch_size=10)
-# print classes
+@app.get("/garbage")
+async def get_garbage_type(garbage_image: bytes = File()):
+    prob, category = predict_garbage_type(garbage_image)
+
+    return {"predicted_type": category, "probability": prob}
